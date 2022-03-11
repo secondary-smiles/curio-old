ecbdfb6b0a969951728bbbaa7aa08b337cccf1f5
smiles_ <65569524+Smiles14@users.noreply.github.com>
2022-03-11 10:11:56 -0500

    removed worlised deps for search function

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,23 +1,29 @@
 var item = document.getElementById("header__navbar__inner-input-parent-input");
-var noResults = document.getElementById("header__navbar__inner-input-parent-noresults");
+var noResults = document.getElementById(
+  "header__navbar__inner-input-parent-noresults"
+);
 
 item.addEventListener("keydown", function (e) {
-    if (e.code === "Enter") {
-        validate();
-    }
+  if (e.code === "Enter") {
+    validate();
+  }
 });
 
-function validate() {
-    text = item.value;
-    text = text.toLowerCase()
-    if (wordlist.includes(text.trim())) {
-        changeWord(text.trim());
-        item.value = "";
-        noResults.style.display = "none";
-        word = text.trim();
-    }
-    else {
-        noResults.style.display = "block";
-    }
-    item.blur();
+async function validate() {
+  text = item.value;
+  text = text.toLowerCase();
+
+  const response = await fetch(
+    "https://secondary-smiles.github.io/curio/words/" + text.trim() + ".html"
+  );
+
+  if (response.status.toString() == '200') {
+    changeWord(text.trim());
+    item.value = "";
+    noResults.style.display = "none";
+    word = text.trim();
+  } else {
+    noResults.style.display = "block";
+  }
+  item.blur();
 }
